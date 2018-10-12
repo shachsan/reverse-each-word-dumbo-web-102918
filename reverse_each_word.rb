c8f209634eb742b09ed7460eb209433d59e025d6
SANJAY K SAI <shachsan@hotmail.com>
2018-10-12 02:13:20 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,5 @@
+
+##Using .each method
 def reverse_each_word(sentence)
   reverse_word = []
   sentence.split.each do |word|
@@ -5,4 +7,9 @@ def reverse_each_word(sentence)
   end
 
   return reverse_word.join(" ")
+end
+
+
+def reverse_each_word(sentence)
+  sentence.split.collect {|word|word.reverse}
 end
